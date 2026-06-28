e30348fe6f37ae369669a35dfba87c082dec4967
bijayu123 <razace70@gmail.com>
2026-06-28 23:03:00 +0545

    Add empty requirements.txt

--- bijay.py
+++
@@ -1,9 +0,0 @@
-import joblib, pickle, os
-model_dir = "saved_models"
-for fname in os.listdir(model_dir):
-    if fname.endswith('.pkl'):
-        filepath = os.path.join(model_dir, fname)
-        with open(filepath, 'rb') as f:
-            model = pickle.load(f)
-        joblib.dump(model, filepath)
-        print(f"✅ Re‑saved: {fname}")
